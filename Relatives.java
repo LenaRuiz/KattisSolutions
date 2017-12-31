b5322c48cdd6d1b2a6d562df6d92cc742b850790
LenaRuiz <34915665+LenaRuiz@users.noreply.github.com>
2017-12-30 17:40:58 -0800

    Update Relatives.java

--- Relatives.java
+++ Relatives.java
@@ -10,6 +10,7 @@ import java.math.*;
 
 public class Relatives {
     public static void main(String[] args) {
+        //Store prime numbers before solving test cases.
         int maxrt = 31626;
         int maxrtrt = 180;
         boolean[] comp = new boolean[maxrt];//sieve of Eritosthenes
@@ -21,32 +22,35 @@ public class Relatives {
                 }
             }
         }
+        
+        //Solve all test cases from stdin
         Scanner in = new Scanner(System.in);
-        int ret = 1;//result, the phi function
         while (in.hasNext()) {
-            ret = 1;
             int n = in.nextInt();
-            if (n == 0) System.exit(0);//end of input
-            if (n == 1) {
-                System.out.println(0);//base case
-                continue;  
-            }
-            int bound = (int)Math.floor(Math.sqrt(n));//to factor n, we only need to check factors up to here
-            for (int i = 2; i <= bound; i++) {
-                if (!comp[i]) {
-                    int count = 0;
-                    while ((n % i) == 0) {
-                        n /= i;
-                        count++;
-                    }
-                    if (count != 0) {//Euler phi of a prime power 
-                        if (count == 1) ret *= (i - 1);
-                        else ret *= (int)(Math.pow(i, count) - Math.pow(i, count - 1));
-                    }
+            if (n == 0) break;//input is termintated by 0
+            System.out.println(eulerPhi(n, comp));
+        }
+    }
+    
+    public static int eulerPhi(int n, boolean[] comp) {
+        if (n == 1) return 0;//end of input
+        int ret = 1;
+        int bound = (int)Math.floor(Math.sqrt(n));//to factor n, we only need to check factors up to here
+        for (int i = 2; i <= bound; i++) {
+            if (!comp[i]) {
+                int count = 0;
+                while ((n % i) == 0) {
+                    n /= i;
+                    count++;
+                }
+                if (count != 0) {//Euler phi of a prime power 
+                    if (count == 1) ret *= (i - 1);
+                    else ret *= (int)(Math.pow(i, count) - Math.pow(i, count - 1));
                 }
             }
-            if (n != 1) ret *= (n - 1);//n may have had one factor greater than sqrt(n)
-            System.out.println(ret);
         }
+        if (n != 1) ret *= (n - 1);//n may have had one factor greater than sqrt(n)
+        
+        return ret;
     }
 } 
